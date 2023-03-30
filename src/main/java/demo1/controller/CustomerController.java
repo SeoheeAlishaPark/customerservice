22de6c3cd27398b2bc8c3503bab57e6b5b1bfe8c
parkseoh <parkseoh@iu.edu>
2023-03-30 13:14:57 -0400

    Initial commit

---
+++ src/main/java/demo1/controller/CustomerController.java
@@ -0,0 +1,52 @@
+package demo1.controller;
+
+import edu.p565.demo1.model.Customer;
+import edu.p565.demo1.repository.CustomerRepository;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customers")
+public class CustomerController {
+
+    private CustomerRepository repository;
+
+    public CustomerController(CustomerRepository repository) {
+        this.repository = repository;
+    }
+
+    // Get https:localhost:8080/customers
+    @GetMapping
+    public List<Customer> findAll(){
+
+        return repository.findAll();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public int create(@Valid @RequestBody Customer customer){
+        Customer newCustomer = repository.save(customer);
+        return newCustomer.getId();
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PutMapping("/{id}")
+    public void update(@Valid @RequestBody Customer customer, @PathVariable int id){
+        customer.setId(id);
+        repository.save(customer);
+    }
+
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable int id){
+
+        Customer customer = new Customer();
+        customer.setId(id);
+        repository.delete(customer);
+    }
+   
+    
+}
---
+++ src/main/java/demo1/repository/CustomerRepository.java
@@ -0,0 +1,6 @@
+package demo1.repository;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+
+}
